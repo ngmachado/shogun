ceba4ab8ca38a947d21bd096dd85642c6a6b7dba
Axe <seamac@gmail.com>
2023-05-31 16:22:44 +0100

    TokenResource fix

--- src/tokens/TokenResources.sol
+++ src/tokens/TokenResources.sol
@@ -2,22 +2,86 @@
 pragma solidity ^0.8.0;
 
 import { ERC1155PresetMinterPauser } from "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 
 contract TokenResources is ERC1155PresetMinterPauser {
 
-    uint256 public constant STONE_ID_1 = 0;
-    uint256 public constant STONE_ID_2 = 1;
-    uint256 public constant STONE_ID_3 = 2;
-    uint256 public constant STONE_ID_4 = 3;
-    uint256 public constant STONE_ID_5 = 4;
+	event SwapTokens(address indexed player, address indexed stoneTokenAddress, uint256 indexed tokenId, uint256 amount);
+	event SetArmyUnitTypeChanged(uint256 indexed tokenId, address indexed stoneTokenAddress, uint16 exchangeRate, uint32 attackPowerUnit, uint32 defensePowerUnit);
 
-	constructor() ERC1155PresetMinterPauser("shogun-stones")  {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-		// Stones
-		_mint(_msgSender(), STONE_ID_1, 1000000 ether, "");
-		_mint(_msgSender(), STONE_ID_2, 1000000 ether, "");
-		_mint(_msgSender(), STONE_ID_3, 1000000 ether, "");
-		_mint(_msgSender(), STONE_ID_4, 1000000 ether, "");
-		_mint(_msgSender(), STONE_ID_5, 1000000 ether, "");
+	struct ArmyUnitType {
+		uint16 stoneToArmyExchangeRate;
+		address stoneTokenAddress;
+		uint32 attackPowerUnit;
+		uint32 defensePowerUnit;
+	}
+
+	uint256 public lastTokenId;
+
+	// mapping from token ID to corresponding ArmyUnitType
+	mapping(uint256 => ArmyUnitType) public tokenIdToArmyUnit;
+
+	// initializes army unit types
+	constructor(address admin, ArmyUnitType[] memory army) ERC1155PresetMinterPauser("armyToken.local") {
+		_setupRole(DEFAULT_ADMIN_ROLE, admin);
+		require(army.length > 0, "army length must be greater than 0");
+		lastTokenId = army.length;
+		for(uint256 i = 0; i < army.length; i++) {
+			setArmyUnit(i+1, army[i]);
+		}
+	}
+
+	// swap stones for army units
+	function swapTokens(uint256 tokenId, uint256 amount) public {
+		ArmyUnitType memory armyUnit = tokenIdToArmyUnit[tokenId];
+		require(armyUnit.stoneTokenAddress != address(0), "Invalid stone token address");
+		require(amount > 0 && amount % (1 ether) == 0, "Amount must represent whole tokens");
+		require(armyUnit.stoneToArmyExchangeRate != 0, "Invalid exchange rate");
+		require(IERC20(armyUnit.stoneTokenAddress).transferFrom(_msgSender(), address(this), amount), "Transfer failed");
+		_mint(_msgSender(), tokenId, amount * armyUnit.stoneToArmyExchangeRate, "");
+	}
+
+	// batch swap tokens
+	function batchSwapTokens(uint256[] memory tokenIds, uint256[] memory amounts) public {
+		require(tokenIds.length == amounts.length, "tokenIds and amounts must have the same length");
+		for(uint256 i = 0; i < tokenIds.length; i++) {
+			swapTokens(tokenIds[i], amounts[i]);
+		}
+	}
+
+	// Set army unit type for given token ID
+	function setArmyUnit(uint256 tokenId, ArmyUnitType memory armyUnit ) public {
+		require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Caller is not an admin");
+		require(isValidArmyUnit(armyUnit), "Invalid army unit");
+		tokenIdToArmyUnit[tokenId] = armyUnit;
+		if(tokenId > lastTokenId) {
+			lastTokenId = tokenId;
+		}
+		emit SetArmyUnitTypeChanged(tokenId, armyUnit.stoneTokenAddress, armyUnit.stoneToArmyExchangeRate, armyUnit.attackPowerUnit, armyUnit.defensePowerUnit);
+	}
+
+	// Helper function to check if an army unit type is valid
+	function isValidArmyUnit(ArmyUnitType memory armyUnit) internal pure returns (bool) {
+		return (
+			armyUnit.stoneToArmyExchangeRate != 0 &&
+			armyUnit.stoneTokenAddress != address(0) &&
+			armyUnit.attackPowerUnit != 0 &&
+			armyUnit.defensePowerUnit != 0
+		);
+	}
+
+	// Return the total attack power and defense power for all army units
+	function getTotalArmyUnits() public view returns (uint256 totalAttackPower, uint256 totalDefensePower) {
+		for(uint256 i = 1; i <= lastTokenId; i++) {
+			ArmyUnitType memory armyUnit = tokenIdToArmyUnit[i];
+			totalAttackPower += armyUnit.attackPowerUnit * balanceOf(_msgSender(), i);
+			totalDefensePower += armyUnit.defensePowerUnit * balanceOf(_msgSender(), i);
+		}
+	}
+
+	// Get an army unit type by token ID
+	function getArmyUnit(uint256 tokenId) public view returns (ArmyUnitType memory) {
+		return tokenIdToArmyUnit[tokenId];
 	}
 }
--- test/TokenResources.t.sol
+++ test/TokenResources.t.sol
@@ -11,16 +11,54 @@ contract TokenResourcesTest is Test {
 
     function setUp() public virtual {
         vm.startPrank(owner);
-        tokenResources = new TokenResources();
+        // create 5 ArmyUnitType in a array
+        TokenResources.ArmyUnitType[] memory armyUnitTypes = new TokenResources.ArmyUnitType[](5);
+        // create 5 ArmyUnitType
+        for (uint i = 0; i < 5; i++) {
+            armyUnitTypes[i] = TokenResources.ArmyUnitType({
+                stoneToArmyExchangeRate: uint16(1),
+                stoneTokenAddress: address(0x01),
+                attackPowerUnit: uint32(10),
+                defensePowerUnit: uint32(100)
+            });
+        }
+        tokenResources = new TokenResources(owner, armyUnitTypes);
     }
 
+    // check if owner has admin role
+    function testOwnerHasAdminRole() public {
+        assertTrue(tokenResources.hasRole(tokenResources.DEFAULT_ADMIN_ROLE(), owner));
+    }
 
-    // test if owner get the right amount of stones
-    function testBalances() public {
-        for(uint256 i = 0; i < 5; i++) {
-            uint256 balance = tokenResources.balanceOf(owner, i);
-            assertEq(balance, 1000000 ether, "wrong balance");
+    // test each ArmyUnitType is created correctly
+    function testArmyUnitType() public {
+        for (uint256 i = 0; i < 5; i++) {
+            TokenResources.ArmyUnitType memory armyUnitType = tokenResources.getArmyUnit(i+1);
+            assertEq(armyUnitType.stoneToArmyExchangeRate, uint16(1));
+            assertEq(armyUnitType.stoneTokenAddress, address(0x01));
+            assertEq(armyUnitType.attackPowerUnit, uint32(10));
+            assertEq(armyUnitType.defensePowerUnit, uint32(100));
         }
+    }
+
+    // set a new ArmyUnitType
+    function testSetArmyUnitType() public {
+
+        TokenResources.ArmyUnitType memory armyUnitType = TokenResources.ArmyUnitType({
+            stoneToArmyExchangeRate: uint16(2),
+            stoneTokenAddress: address(0x02),
+            attackPowerUnit: uint32(20),
+            defensePowerUnit: uint32(200)
+        });
+        uint256 lastTokenId = tokenResources.lastTokenId();
+        tokenResources.setArmyUnit(lastTokenId + 1, armyUnitType);
+        armyUnitType = tokenResources.getArmyUnit(lastTokenId + 1);
+        assertEq(armyUnitType.stoneToArmyExchangeRate, uint16(2));
+        assertEq(armyUnitType.stoneTokenAddress, address(0x02));
+        assertEq(armyUnitType.attackPowerUnit, uint32(20));
+        assertEq(armyUnitType.defensePowerUnit, uint32(200));
+
+        assertEq(tokenResources.lastTokenId(), lastTokenId + 1);
     }
 
 }
